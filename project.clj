803a26d02544c6b202bf557a31d999618106a937
Matt Gauger <matt.gauger@gmail.com>
2014-01-12 18:08:59 -0600

    Temporary Vagrant setup

--- project.clj
+++ project.clj
@@ -6,11 +6,4 @@
   :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.palletops/pallet "0.8.0-RC.5"]
-                 [pallet-hadoop "0.3.3-beta.2"]
-                 [org.cloudhoist/pallet-jclouds "1.4.2"]
-                 [org.jclouds/jclouds-all "1.4.2"]
-                 [org.jclouds.driver/jclouds-jsch "1.4.2"]
-                 [org.jclouds.driver/jclouds-slf4j "1.4.2"]
-                 [ch.qos.logback/logback-classic "1.0.1"]
-                 [ch.qos.logback/logback-core "1.0.1"]
-                 [com.palletops/pallet-vmfest "0.3.0-beta.2"]])
+                 [com.palletops/pallet-vmfest "0.3.0-beta.2" :exclusions [org.clojure/tools.logging]]])
