973671f6a94db840feec900028bb39ff8ad83d37
Matt Gauger <matt.gauger@gmail.com>
2014-02-01 17:44:43 -0600

    Add back pallet-hadoop

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.palletops/pallet "0.8.0-RC.6"]
+                 [org.cloudhoist/pallet-hadoop "0.3.3"]
                  [com.palletops/pallet-vmfest "0.4.0-alpha.1"]
                  [ch.qos.logback/logback-classic "1.0.9"]
                  [org.slf4j/jcl-over-slf4j "1.7.3"]])
