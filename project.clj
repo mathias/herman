92d1c93d72fdb545cbeff31e5ddefcf8df546f96
Matt Gauger <matt.gauger@gmail.com>
2014-02-01 17:41:46 -0600

    Can successfully install vim with Pallet
    - This means we can continue provisioning with pallet. huzzah!

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/mathias/herman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/releases/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.palletops/pallet "0.8.0-RC.5"]
-                 [com.palletops/pallet-vmfest "0.3.0-beta.2" :exclusions [org.clojure/tools.logging]]])
+                 [com.palletops/pallet "0.8.0-RC.6"]
+                 [com.palletops/pallet-vmfest "0.4.0-alpha.1"]
+                 [ch.qos.logback/logback-classic "1.0.9"]
+                 [org.slf4j/jcl-over-slf4j "1.7.3"]])
--- src/herman/core.clj
+++ src/herman/core.clj
@@ -1,6 +1,19 @@
-(ns herman.core)
+(ns herman.core
+  (:require [pallet.actions :refer [exec package]]
+            [pallet.api :refer [group-spec lift plan-fn converge group-spec]]
+            [pallet.core.user :refer [make-user]]
+            [pallet.compute :refer [instantiate-provider nodes]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def hadoop-ip "10.10.10.10")
+
+(def hadoop-provider
+  (instantiate-provider
+   "node-list"
+   :node-list [["hadoop" "hadoop-master" hadoop-ip :ubuntu]]))
+
+(defn install-vim []
+  (lift
+   (group-spec "hadoop-master"
+               :phases {:configure (plan-fn (package "vim"))})
+   :compute hadoop-provider
+   :timeout-ms 10000))
