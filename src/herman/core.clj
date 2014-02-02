72998924a60adefe5f1fd5c6d760db9001bc7e88
Matt Gauger <matt.gauger@gmail.com>
2014-02-01 20:35:50 -0600

    WIP: Hadoop almost fully installed.

--- src/herman/core.clj
+++ src/herman/core.clj
@@ -1,6 +1,8 @@
 (ns herman.core
-  (:require [pallet.action.package :refer [package]]
+  (:require [pallet.action.package :refer [package package-manager]]
             [pallet.core :refer [group-spec lift converge]]
+            [pallet-hadoop.node :refer [lift-cluster cluster-spec node-group]]
+            [pallet.crate.hadoop :as hadoop]
             [pallet.compute :refer [compute-service]]
             [pallet.phase :refer [phase-fn]]
             [pallet.compute.node-list :refer [node-list-service make-node]]))
@@ -16,4 +18,24 @@
   (lift
    (group-spec "hadoop-master"
                :phases {:configure (phase-fn (package "vim"))})
+   :compute hadoop-provider))
+
+(def hadoop-cluster
+  (cluster-spec :public
+                {:master (node-group [:jobtracker :namenode :slavenode])
+                 :base-machine-spec {:os-family :ubuntu
+                                     :os-version-matches "13.10"
+                                     :os-64-bit true}
+                 :base-props {:mapred-site {:mapred.task.timeout 300000
+                                            :mapred.reduce.tasks 3
+                                            :mapred.tasktracker.map.tasks.maximum 3
+                                            :mapred.tasktracker.reduce.tasks.maximum 3
+                                            :mapred.child.java.opts "-Xms1024m"}}}))
+
+(defn configure-cluster []
+  (lift
+   (group-spec "hadoop-master"
+               :phases {:configure (phase-fn (package-manager :update)
+                                             (pallet.crate.java/java :openjdk)
+                                             (hadoop/install  :cloudera))})
    :compute hadoop-provider))
